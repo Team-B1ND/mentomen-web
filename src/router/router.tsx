78ed20f23e3b27af271fe7a7ce8dc078b89b8bdd
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-16 15:44:29 +0900

    ⚡️ :: detail 라우터 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Detail from "../components/detail";
 import AuthLoadingPage from "../pages/auth/AuthLoadingPage";
 import HomePage from "../pages/HomePage";
 import IntroducePage from "../pages/IntroducePage";
@@ -11,6 +12,7 @@ function Router() {
         <Route path="/start" element={<StartPage />} />
         <Route path="/callback" element={<AuthLoadingPage />} />
         <Route path="/intro" element={<IntroducePage />} />
+        <Route path='/detai:id' element={<Detail/>} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </>
