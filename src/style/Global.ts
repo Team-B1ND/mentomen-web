b44c9387826e7909863d3b0475534677ff135d96
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-11-22 17:45:54 +0900

    ⚡️ :: pretendara 폰트 적용

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -1,12 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import "./font.css";
 
 const GlobalStyle = createGlobalStyle`
     * {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
-      font-family: 'Pretendard' !important;
+      font-family: 'Pretendard-Regular' !important;
      }
     ${reset}
 `;
