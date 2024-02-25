81adb33a9dab47816880135f36892a132fbb0165
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-02-25 21:02:30 +0900

    refactor: DefaultSeo 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import "@/src/styles/font.css";
 import useGATracker from "../hooks/Analyze/useGATracker";
-import { DefaultSeo, NextSeo } from "next-seo";
+import { DefaultSeo } from "next-seo";
 import { useNextSeoConfig } from "../hooks/SEO/useNextSeoConfig";
 
 const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
@@ -16,6 +16,10 @@ const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
 }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
   const [isClient, setIsClient] = useState(false);
+  const { SeoConfigProps } = useNextSeoConfig({
+    title: "멘투멘 - 멘토와 멘티를 잇다",
+    description: "멘토에게 궁금하거나 알고 싶었던 전공지식에 관해 물어보세요!",
+  });
 
   useEffect(() => {
     setIsClient(true);
@@ -25,6 +29,7 @@ const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
 
   return (
     <>
+      <DefaultSeo {...SeoConfigProps} />
       {isClient && (
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
